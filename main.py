3c0d19d746621b3a6da5253e37f2f4af70bdfa7b
Dirk Fraanje <46317263+dirkfraanje@users.noreply.github.com>
2023-05-26 20:45:39 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+def user_input():
+    day = input('Enter day number: ')
+    part = input('Enter 1 or 2 for part number: ')
+    execute_day(day, part)
+
+
+def execute_day(day, part):
+    if int(part) == 1 or int(part) == 2:
+        print(f'Executing part {part} of day {day}')
+        print('')
+        user_input()
+    else:
+        print(f'{part} is not a valid number for part, choose either 1 or 2..')
+        user_input()
+
+
+user_input()
