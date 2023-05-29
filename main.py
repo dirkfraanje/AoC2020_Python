452fdba4496a3270ab1f0f806efa39245393aa96
Dirk Fraanje <46317263+dirkfraanje@users.noreply.github.com>
2023-05-29 21:54:57 +0200

    Solution for day 1, part 2 Added console colors Added max day

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from day1 import Day1
+from colorama import init as colorama_init
+from colorama import Fore
+from colorama import Style
+
+colorama_init()
+current_day_number_max = 1
 
 
 def user_input():
     day = input('Enter day number: ')
-    part = input('Enter 1 or 2 for part number: ')
-    execute_day(day, part)
+    if not day or int(day) > current_day_number_max:
+        print(f'{Fore.RED}Current maximum day number is {current_day_number_max}{Style.RESET_ALL}')
+        print('')
+        user_input()
+    else:
+        part = input('Enter 1 or 2 for part number: ')
+        execute_day(day, part)
 
 
 def execute_day(day, part):
@@ -14,7 +25,8 @@ def execute_day(day, part):
         print('')
         user_input()
     else:
-        print(f'{part} is not a valid number for part, choose either 1 or 2..')
+        print((f'{Fore.RED}{part} is not a valid number for part, choose either 1 or 2..{Style.RESET_ALL}'))
+        print('')
         user_input()
 
 
