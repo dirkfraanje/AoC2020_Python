16944265f0adcb998a9fa0eb13dd249f24e98453
Dirk Fraanje <46317263+dirkfraanje@users.noreply.github.com>
2023-06-03 21:22:21 +0200

    Solution for day 2, part 2

--- day2.py
+++ day2.py
@@ -7,14 +7,11 @@ def part1():
     valid_passwords = 0
     with open('day2.txt') as input_1:
         for line in input_1:
-            if password_is_valid(line):
+            if password_is_valid_part_1(line):
                 valid_passwords += 1
     print(f'Number of valid passwords: {Fore.GREEN}{valid_passwords}{Style.RESET_ALL}')
 
-def part2():
-    print(f'{Fore.LIGHTRED_EX}Work in progress...{Style.RESET_ALL}')
-
-def password_is_valid(line):
+def password_is_valid_part_1(line):
     splitted_line = line.split()
     range_minimum = int(splitted_line[0].split('-')[0])
     range_maximum = int(splitted_line[0].split('-')[1])
@@ -24,6 +21,23 @@ def password_is_valid(line):
 
     return range_minimum <= required_character_occurrences <= range_maximum
 
+
+def part2():
+    valid_passwords = 0
+    with open('day2.txt') as input_1:
+        for line in input_1:
+            if password_is_valid_part_2(line):
+                valid_passwords += 1
+    print(f'Number of valid passwords: {Fore.GREEN}{valid_passwords}{Style.RESET_ALL}')
+
+def password_is_valid_part_2(line):
+    splitted_line = line.split()
+    position_1 = int(splitted_line[0].split('-')[0])
+    position_2 = int(splitted_line[0].split('-')[1])
+    required_character = splitted_line[1][0]
+    password = splitted_line[2]
+
+    return (password[position_1-1] == required_character and password[position_2-1] != required_character) or (password[position_2-1] == required_character and password[position_1-1] != required_character)
 
 class Day2:
     def __init__(self, part):
